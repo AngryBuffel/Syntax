53772e25f2ec0a001facbd21f66443a1948096c9
AngryBuffel <dls0612+github@gmail.com>
2018-12-06 00:08:05 -0600

    Ex update,finish

--- Ex_C5_PCC.py
+++ Ex_C5_PCC.py
@@ -1,6 +1,29 @@
 # VDLS - 11/25/18 - Excercises Chaper 5 Python Crash Course
 # 5.1 - Conditional tests.
-age = 21
-cute = True
-print('Is Kari 21 yo old?')
-print(age == 21)
+import random
+alien_color = ['green','red','blue','yellow','magenta','cyan']
+
+def rand_pick(colors):
+    # Generates a random index to pick a color.
+    return colors[random.randint(0,len(colors))]
+
+#print(alien_pick(alien_color))
+score = 0
+print('Do you want to choose an alien color? y/n \n')
+choice = input()
+
+while choice == 'y':
+    if rand_pick(alien_color) == 'green':
+        score += 5
+        print('Alien was green, you win 5 points!')
+    elif rand_pick(alien_color) == 'yellow':
+        score += 10
+        print('Alien was yellow, you win 10 points!')
+    elif rand_pick(alien_color) == 'red':
+        score += 15
+        print('Alien was red, you win 15 points!')
+    else:
+        score += 7
+        print('Alien was a custom color, you win 7 points!')
+    print('Your score is ' + str(score) + ' points!')
+    choice = input('Do you want to choose another alien color? y/n')
