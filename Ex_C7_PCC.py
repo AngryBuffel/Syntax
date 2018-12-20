0121a112887f8ff17a002eadca8e97e57b3c5c9c
AngryBuffel <dls0612+github@gmail.com>
2018-12-20 00:59:48 -0600

    finish 7

--- Ex_C7_PCC.py
+++ Ex_C7_PCC.py
@@ -1,5 +1,11 @@
 # VDLS - 12/18/18 - Excercises Chaper 7 Python Crash Course
 # 7.1 Rental Car
 car_model = input('Which car brand would you like to use?: ')
-print('Let me see if I can find you a ' + car_model + '.')
+print('Let me see if I can find you a ' + car_model.title() + '.')
 # 7.2 Restaurant Seating
+people_num = input('How many people are in your dinner group?: ')
+people_num = int(people_num)
+if people_num > 8:
+    print("You'll have to wait for a table...")
+else:
+    print("Your table is ready!")
