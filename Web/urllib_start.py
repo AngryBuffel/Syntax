60838356adbfecbb741f2215e6ff5f0a28bafb25
AngryBuffel <dls0612+github@gmail.com>
2018-12-22 00:45:30 -0600

    finish lynda tut

--- Web/urllib_start.py
+++ Web/urllib_start.py
@@ -5,7 +5,7 @@ import urllib.request
 def main():
     # URL to retrieve sample data from
     url = "http://httpbin.org/xml"
-
+    # Issue request with data params as part of the url
     result = urllib.request.urlopen(url)
 
     print("Result code: {0}".format(result.status))
