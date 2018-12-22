c7583c08c320252771db2631e109f4ca55da1eaa
AngryBuffel <dls0612+github@gmail.com>
2018-12-21 22:03:11 -0600

    new folder

---
+++ Web/urllib_start.py
@@ -0,0 +1,21 @@
+# Using urllib to request data
+# TODO: import urllib request class
+import urllib.request
+
+def main():
+    # URL to retrieve sample data from
+    url = "http://httpbin.org/xml"
+
+    result = urllib.request.urlopen(url)
+
+    print("Result code: {0}".format(result.status))
+
+    print("Headers: ----------------------")
+    print(result.getheaders())
+
+    print("Returned data: ----------------")
+    print(result.read().decode('utf-8'))
+
+
+if __name__ == "__main__":
+    main()
