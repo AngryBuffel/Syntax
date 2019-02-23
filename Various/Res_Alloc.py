9204f18c79d81b9911cee937aa27f5a268bd55ce
Work_PC <victor.liceaga@here.com>
2019-02-23 00:57:41 -0600

    alloc updt

--- Various/Res_Alloc.py
+++ Various/Res_Alloc.py
@@ -9,6 +9,7 @@
 
 # Libraries import
 
+import sys
 import numpy as np
 import pandas as pd
 
@@ -22,19 +23,27 @@ ws_df = pd.read_csv(ws_file_loc)
 # Define matrices to multiply
 
 rs = user_df.iloc[:,3:198]
-ws = ws_df.iloc[0:-1,:28]
+ws = ws_df.iloc[:-1,1:-1]
 
 # Define weight vector, TODO: add column to ws_skill_matrix to use as weight vector /Done 
 
-wv = ws_df.weight[:195]
+wv = ws_df.iloc[:-1,-1]
 
-# Multiply WS matrix by wieght vector
+# Multiply WS matrix by wv
 
-ws.apply(lambda x: np.asarray(x) * np.asarray(wv))
+ws_w = ws.mul(wv, axis = 0)
 
-print(ws)
+# Calculate Allocation Matrix #TODO: Reindex am by id_emp /Done
+# np.dot(rs,ws_w) dot is easier to carry out as np but then we would need to convert back to df
 
-result = rs.dot(ws.set_index('Skills'))
+am = rs.dot(ws_w.set_index(ws_df.iloc[:195,0])).set_index(user_df.iloc[:215,0])
 
-print(result)
+print(am)
 
+#TODO: Calculate # of resources to assign to each WS
+# Add as a bottom row at ws_df? ws_df.loc[199] = [Generated vector with corresponding values]
+
+#TODO: WS priority list to assign resources /Done, added as last row in ws_df
+#TODO: Loop through WS priority list allocating resources by sorting corresponding am column
+#TODO: Pick top resources needed from sorted column to match capacity needs, assing to WS
+#TODO: Remove allocated resources by id_emp from am matrix
